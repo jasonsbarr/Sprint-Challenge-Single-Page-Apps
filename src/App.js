412bed1a9ffd0fece7fa7ad9b33554c2fe228f35
Jason Barr <jason@jasonsbarr.com>
2019-10-12 13:36:49 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "./components/Header.js";
 import { Route } from "react-router-dom";
 import Characters from "./pages/Characters.js";
+import WelcomePage from "./pages/WelcomePage";
 
 export default function App() {
   return (
     <main>
       <Header />
 
-      <Route to="/characters" component={Characters} />
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={Characters} />
     </main>
   );
 }
