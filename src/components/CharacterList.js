d9e9bce39e95734994e052c5a072817a98fa7eef
Jason Barr <jason@jasonsbarr.com>
2019-10-12 12:56:57 -0500

    Refactor CharacterList to receive data from container

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
-
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
-
+const CharacterList = ({ characters }) => {
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <pre>{JSON.stringify(characters, null, 2)}</pre>
     </section>
   );
-}
+};
+
+export default CharacterList;
