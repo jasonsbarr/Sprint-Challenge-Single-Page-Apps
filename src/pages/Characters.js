f7648a0d4391304d9e88700d6ac99f2fc8ac6497
Jason Barr <jason@jasonsbarr.com>
2019-10-12 12:48:44 -0500

    Set up state and data fetching for characters list

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,7 +1,28 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Characters = () => {
   const [characters, setCharacters] = useState(null);
+  const url = "https://rickandmortyapi.com/api/character/";
+
+  useEffect(() => {
+    const getCharacters = () => {
+      axios
+        .get("https://rickandmortyapi.com/api/character/")
+        .then(response => {
+          setCharacters(response.data);
+        })
+        .catch(error => {
+          console.error("Server Error", error);
+        });
+    };
+
+    getCharacters();
+  }, []);
+
+  return (
+    <pre>{JSON.stringify(characters, null, 2) || "Loading..."}</pre>
+  );
 };
 
 export default Characters;
