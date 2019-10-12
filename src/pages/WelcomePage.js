925817efcf22167c417d951ee45ff2832eb0a01d
Jason Barr <jason@jasonsbarr.com>
2019-10-12 13:35:55 -0500

    Move WelcomePage to /pages

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const WelcomePage = () => (
   <section className="welcome-page">
@@ -10,6 +11,9 @@ const WelcomePage = () => (
         alt="rick"
       />
     </header>
+    <p>
+      <Link to="/characters">View Characters &rarr;</Link>
+    </p>
   </section>
 );
 
