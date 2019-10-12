b9bd434e199ffcf8df36a236524acd11b808aeae
Jason Barr <jason@jasonsbarr.com>
2019-10-12 12:48:24 -0500

    Add route for Characters page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import { Route } from "react-router-dom";
+import Characters from "./pages/Characters.js";
 
 export default function App() {
   return (
     <main>
       <Header />
+
+      <Route to="/characters" component={Characters} />
     </main>
   );
 }
