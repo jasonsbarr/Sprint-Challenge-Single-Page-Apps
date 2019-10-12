46cfa0ead8cdd5f1dd23c0b4bbe8296ef234fbb1
Jason Barr <jason@jasonsbarr.com>
2019-10-12 13:15:44 -0500

    Conditionally render list to avoid calling map on null

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,12 +2,14 @@ import React from "react";
 import CharacterCard from "./CharacterCard";
 
 const CharacterList = ({ characters }) => {
-  return (
+  return characters ? (
     <section className="character-list">
       {characters.map(character => (
         <CharacterCard key={character.id} character={character} />
       ))}
     </section>
+  ) : (
+    <div className="loading">Loading...</div>
   );
 };
 
