65190374b5113d3f985f6bbb127a21ddeb92d831
Jason Barr <jason@jasonsbarr.com>
2019-10-12 13:04:12 -0500

    Fill out character details

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,29 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+const CharacterCard = ({ character }) => {
+  const {id, name, gender, species, status, image, origin.name};
+  return (
+    <div>
+      <div className="media-image-container">
+        <img
+          className="media-image"
+          src={image}
+          alt={name}
+        />
+        <p className="media-image-caption">
+          {`${name}, status: ${status}`}
+        </p>
+      </div>
+      <div className="media-details">
+        <h2 className="media-details-heading">{name}</h2>
+        <p className="media-card-label">Known facts:</p>
+        <ul className="media-card-list">
+          <li className="media-card-list-item">{`Species: ${species}`}</li>
+          <li className="media-card-list-item">{`From: ${origin.name}`}</li>
+          <li className="media-card-list-item">{`Gender: ${gender}`}</li>
+          <li className="media-card-list-item">{`Status: ${status}`}</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
